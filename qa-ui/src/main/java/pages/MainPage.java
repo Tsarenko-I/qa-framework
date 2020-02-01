753610b75436411cd71a45d90012b4c16408879a
tsarenko <tsaarenk@gmail.como>
2020-02-01 02:57:09 +0200

    fix allure

--- qa-ui/src/main/java/pages/MainPage.java
+++ qa-ui/src/main/java/pages/MainPage.java
@@ -4,11 +4,14 @@ package pages;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.open;
+import org.testng.asserts.*;
 
 public class MainPage {
 
     @Step("Open browser and Main Page")
     public void openMainPage(String urlMainPage) {
         open(urlMainPage);
+        Assertion assertion = new Assertion();
+        assertion.assertTrue(false);
     }
 }
